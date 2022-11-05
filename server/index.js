7e4f8c99b698939a28cfdb83632f3583ba48141e
Tuomo Torppa <tuomo.torppa@gmail.com>
2022-11-05 11:27:25 +0200

    fly.io auto publish,

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors')
+const path = require('path');
 
-app.get('/ping', (req, res) => {
+const app = express()
+const port = 8080
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+app.get('/api/ping', (req, res) => {
   res.send('Hello World!')
 })
 
