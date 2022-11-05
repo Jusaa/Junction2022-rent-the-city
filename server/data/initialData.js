7e04b565c0a9a95a521a70f8dcc302a1ff07a93e
Pyry Piirainen <PyryPiirainen@users.noreply.github.com>
2022-11-05 14:23:02 +0200

    more tables, foreign key setting working

--- server/data/initialData.js
+++ server/data/initialData.js
@@ -1,9 +1,5 @@
 const categories = [
     {
-        name: 'Tools',
-        imageUrl: '/img/tools.jpg',
-    },
-    {
         name: 'Toys',
         imageUrl: '/img/toys.jpg',
     },
@@ -18,7 +14,8 @@ const bookableItems = [
         name: 'Makita drill',
         description: 'Very good drill for making holes.',
         imageUrl: '/img/makitaDrill.jpg',
-    }
+        categories: [ { name: 'Tools', imageUrl: '/img/tools.jpg' }, ]
+    },
 ];
 
 module.exports = {
