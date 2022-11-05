6f6ca1a30783045c50f5c0d8c6d29cfc894a3e91
Pyry Piirainen <PyryPiirainen@users.noreply.github.com>
2022-11-05 16:14:41 +0200

    Added lender, borrower, addresses

--- server/data/initialData.js
+++ server/data/initialData.js
@@ -20,9 +20,41 @@ const bookableItems = [
 
 const users = [
     {
-        name: 'Test Testerson',
-        email: 'test.testerson@test.com',
+        name: 'Test Lender',
+        email: 'test.lender@test.com',
         phoneNumber: '+3581234567',
+    },
+    {
+        name: 'Test Borrower',
+        email: 'test.borrower@test.com',
+        phoneNumber: '+3588791234',
+    }
+];
+
+const addresses = [
+    {
+        street: 'Arkadiankatu 3-6',
+        city: 'Helsinki',
+        postalCode: '00100',
+        deliveryComment: 'Wolt office in 3rd floor',
+    },
+    {
+        street: 'Keilaranta 4',
+        city: 'Espoo',
+        postalCode: '02150',
+        deliveryComment: 'Take the stairs to office in second floor.',
+    }
+];
+
+const lenders = [
+    {
+        merchantName: 'My Merchant',
+    }
+];
+
+const borrowers = [
+    {
+        publicUsername: 'Borrower2000',
     }
 ];
 
@@ -30,4 +62,7 @@ module.exports = {
     categories,
     bookableItems,
     users,
+    lenders,
+    borrowers,
+    addresses,
 }
