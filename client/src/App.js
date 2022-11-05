cd7cbdf05c2d85855bcd6d492ae5fd9a42ba8457
Jussi Liukkonen <jussi.liukkonen@crowdcollective.com>
2022-11-05 20:22:52 +0200

    state yes

--- client/src/App.js
+++ client/src/App.js
@@ -14,17 +14,33 @@ import LenderHome from './views/lenderHome'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: 'Guest', role: 'borrower' };
+    this.state = { user: { name: '', role: 'borrower' }};
+    this.setUsername = this.setUsername.bind(this)
+    this.setRole = this.setRole.bind(this)
+    this.getUser = this.getUser.bind(this)
   }
+
+  setUsername(name) {
+    this.setState({ user: { name, role: this.state.user.role }})
+  }
+
+  setRole(role) {
+    this.setState({ user: { name: this.state.user.name, role }})
+  }
+
+  getUser() {
+    return this.state.user
+  }
+
   render() {
     return (
       <Router>
         <Routes>
-          <Route path="/login" element={<Login state={this.state}/>}></Route>
-          <Route path="/signup" element={<Signup state={this.state}/>}></Route>
-          <Route path="/borrower/home" element={<BorrowerHome state={this.state}/>}></Route>
-          <Route path="/lender/home" element={<LenderHome state={this.state}/>}></Route>
-          <Route path="/" element={<MainPage state={this.state}/>}></Route>
+          <Route path="/login" element={<Login setUsername={this.setUsername} setRole={this.setRole} getUser={this.getUser}/>}></Route>
+          <Route path="/signup" element={<Signup setUsername={this.setUsername} setRole={this.setRole} getUser={this.getUser}/>}></Route>
+          <Route path="/borrower/home" element={<BorrowerHome getUser={this.getUser}/>}></Route>
+          <Route path="/lender/home" element={<LenderHome getUser={this.getUser}/>}></Route>
+          <Route path="/" element={<MainPage getUser={this.getUser}/>}></Route>
         </Routes>
       </Router>
     );
