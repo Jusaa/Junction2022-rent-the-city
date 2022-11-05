d6a70449f2ecadef984a7c406459234967c48544
Tuomo Torppa <tuomo.torppa@gmail.com>
2022-11-05 13:18:18 +0200

    Api reqs in progess

--- server/index.js
+++ server/index.js
@@ -41,6 +41,20 @@ app.get('/api/reset', async (req, res) => {
   }
 });
 
+app.get('/api/categories/:id/bookableitems', (req, res) => {
+    const { id } = req.params;
+
+});
+
+app.post('/api/categories/:id/bookableitems', (req, res) => {
+    const { name, description, imageUrl } = req.body;
+    if (!name || !description || !imageUrl || !categoryId ) {
+        res.status(400).send({ error: 'missing params'});
+    }
+
+    res.send('Pigngg!')
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
